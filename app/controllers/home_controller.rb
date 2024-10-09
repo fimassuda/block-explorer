818aeaf929eb41078dfd7cb73049ca755bdf5c02
Fillipe Massuda <fi.massuda@gmail.com>
2024-10-09 10:49:22 -0700

    adds ui to show all transfers and average gas_burnt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @transactions = Transaction.all
+    @transfers = Action.includes(:block_transaction).where(action_type: ACTION::TRANSFER).order(created_at: :desc)
+    @average_gas_burnt = Transaction.average(:gas_burnt)
   end
 end
